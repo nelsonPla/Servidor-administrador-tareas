0e131ab49e4c839d433f87f4612f1ed48765a9fd
nelsonPla <65683458+nelsonPla@users.noreply.github.com>
2020-08-08 17:08:08 -0600

    agregando cors

--- servidor/routes/auth.js
+++ servidor/routes/auth.js
@@ -3,13 +3,20 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { check } = require('express-validator');
+const auth = require('../middleware/auth');
 
-//crear usuario
+//Iniciar session
 //api/auth
-router.post('/', [
-        check('email', 'Agrega un email valido').isEmail(),
-        check('password', 'El password debe ser minimo 6 caracteres').isLength({ min: 6 })
-    ],
+router.post('/',
+    /*[
+           check('email', 'Agrega un email valido').isEmail(),
+           check('password', 'El password debe ser minimo 6 caracteres').isLength({ min: 6 })
+       ],*/
     authController.autenticarUsuario
+);
+//obtiene el usuario autenticado
+router.get('/',
+    auth,
+    authController.usuarioAutenticado
 );
 module.exports = router;
